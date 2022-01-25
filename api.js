40cf98d88d0e20350735fb42482c69de8ce79251
Nathan Orsmond <orsmondn@hotmail.com>
2022-01-25 18:36:18 +0000

    Added functionality for PUT request to update an order

--- api.js
+++ api.js
@@ -15,7 +15,6 @@ const connection = mysql.createConnection({
     database: process.env.DATABASE
 });
 
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -23,32 +22,39 @@ app.use(cors());
 
 app.post('/order', (req, res) => {
     const order = req.body;
-    connection.connect();
-        connection.query('INSERT INTO orders (orderDate, manufacturer, model, price) VALUES (?, ?, ?, ?)', [order.date, order.manufacturer, order.model, order.price], function(err, result) {
-            if(err) throw err;
-            let oid = result.insertId;
-            if(res.statusCode === 200) {
-                res.send("You have successfully created an order. The order id is: " + oid);
-            }
-        });
-    connection.end();
+    connection.query('INSERT INTO orders (orderDate, manufacturer, model, price) VALUES (?, ?, ?, ?)', [order.date, order.manufacturer, order.model, order.price], function(err, result) {
+        if(err) throw err;
+        let oid = result.insertId;
+        if(res.statusCode === 200) {
+            res.send("You have successfully created an order. The order id is: " + oid);
+        }
+    });
 });
 
 app.get('/order/:id', (req, res) => {
     const oid = req.params.id;
-    connection.connect();
-        connection.query(`SELECT orderId, orderDate, manufacturer, model, price FROM orders WHERE orderId = ${oid}`, function(err, result) {
-            if(err) throw err;
-            if(res.statusCode === 200) {
-                res.send(result);
-            }
-        });
+    connection.query(`SELECT orderId, orderDate, manufacturer, model, price FROM orders WHERE orderId = ${oid}`, function(err, result) {
+        if(err) throw err;
+        if(res.statusCode === 200) {
+            res.send(result);
+        }
+    });
 }); 
 
-// app.put(`/updateorder/${oid}`, (req, res) => {
-//     const 
-// });
+app.put(`/updateorder/:id`, (req, res) => {
+    const oid = req.params.id;
+    const updatedValues = req.body;
+    connection.query(`UPDATE orders 
+                SET manufacturer = ?, model = ?, price = ?
+                WHERE orderId = ${oid}`,
+                [updatedValues.manufacturer, updatedValues.model, updatedValues.price], function(err, result) {
+                    if(err) throw err;
+                    if(res.statusCode === 200) {
+                        res.send("You have successfully updated the order.");
+                    }
+    });
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log("Technical test app is running");
 });
