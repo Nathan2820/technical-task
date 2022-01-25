9408dd01d5e6a7d4a5c67acf9d1bb95eef6a062b
Nathan Orsmond <orsmondn@hotmail.com>
2022-01-25 15:37:10 +0000

    added database mysql database connection into api.js file and established connection to local mysql storage for testing purposes. Added dotenv to allow use of environment variables for establishing database connection to cloud services in future

--- api.js
+++ api.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.DBUSER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+});
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -12,12 +23,28 @@ app.use(cors());
 
 app.post('/order', (req, res) => {
     const order = req.body;
+    connection.connect();
+        connection.query('INSERT INTO orders (orderDate, manufacturer, model, price) VALUES (?, ?, ?, ?)', [order.date, order.manufacturer, order.model, order.price], function(err, result) {
+            if(err) throw err;
+            let oid = result.insertId;
+            if(res.statusCode === 200) {
+                res.send("You have successfully created an order. The order id is: " + oid);
+            }
+        })
+    connection.end();
     console.log(order);
-    if(res.statusCode === 200) {
-        res.send("You have successfully created an order.");
-    }
 });
 
+app.get(`/order/:oid`, (req, res) => {
+    let oid = req.params.oid;
+    const order = res.body;
+    console.log(order);
+}); 
+
+// app.put(`/updateorder/${oid}`, (req, res) => {
+//     const 
+// });
+
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+    console.log(`Example app listening on port ${port}`);
+});
